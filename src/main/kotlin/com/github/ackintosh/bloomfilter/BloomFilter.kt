82e801c4863ede911df4ea520cd5ad8527e0bf1c
ackintosh <sora.akatsuki@gmail.com>
2019-06-05 21:10:46 +0900

    rename

--- src/main/kotlin/com/github/ackintosh/bloomfilter/BloomFilter.kt
+++ src/main/kotlin/com/github/ackintosh/bloomfilter/BloomFilter.kt
@@ -15,7 +15,7 @@ class BloomFilter(private val size: Int) {
     fun add(value: ByteArray) {
         lock.write {
             for (seed in 1..3) {
-                bits.set(hash.hash(seed, value))
+                bits.set(hash.call(seed, value))
             }
         }
     }
@@ -23,7 +23,7 @@ class BloomFilter(private val size: Int) {
     fun mightContains(value: ByteArray): Boolean {
         lock.read {
             for (seed in 1..3) {
-                if (!bits.get(hash.hash(seed, value))) {
+                if (!bits.get(hash.call(seed, value))) {
                     return false
                 }
             }
@@ -34,7 +34,7 @@ class BloomFilter(private val size: Int) {
     private class Hash(val filterSize: Int) {
         private val md5 = MessageDigest.getInstance("MD5")
 
-        fun hash(seed: Int, value: ByteArray) =
+        fun call(seed: Int, value: ByteArray) =
             BigInteger(1, md5.digest(value + seed.toByte()))
                 .remainder(BigInteger.valueOf(filterSize.toLong()))
                 .toInt()
